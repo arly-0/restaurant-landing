0f53d0fc2e932a9ec3d2aaa8a00b391ec8c76a1b
Artur <arturworkit@gmail.com>
2023-11-04 20:23:38 +0200

    Create awards section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-
-import './Laurels.css';
+import React from 'react'
+import { images, data } from '../../constants'
+import './Laurels.css'
+import { SubHeading } from '../../components'
 
 const Laurels = () => (
-  <div>
-    Laurels
-  </div>
-);
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Awards & Recognition' />
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+      <div className='app__laurels_awards'>
+        {data.awards.map(award => <AwardCard award={award} key={award.title} />)}
+      </div>
+    </div>
 
-export default Laurels;
+    <div className='app__wrapper_img'>
+      <img src={images.laurels} alt="laurels" />
+    </div>
+  </div>
+)
+
+export default Laurels
+
+
+
+const AwardCard = ({ award }) => (
+  <div className='app__laurels_awards-card'>
+    <img src={award.imgUrl} alt="award" />
+    <div className='app__laurels_awards-card_content'>
+      <p className='p__cormorant' style={{ color: 'var(--color-golden)' }}>{award.title}</p>
+      <p className='p__cormorant'>{award.subtitle}</p>
+    </div>
+  </div>
+)
